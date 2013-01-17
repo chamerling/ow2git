e3ddd98496c61d9da726c77e2c3ef31d32d97926
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-01-17 11:23:42 +0100

    Initial version

---
+++ ow2git.js
@@ -0,0 +1,51 @@
+/**
+ * OW2 API Sample
+ *
+ * Copyright(c) 2013 Christophe Hamerling <christophe.hamerling@gmail.com>
+ * MIT Licensed
+ */
+ 
+require('shelljs/global');
+var _ = require('underscore')
+  , argv = require('optimist').argv
+  , colors = require('colors');
+
+var cli = function() {
+  console.log(argv.clone)
+  if (argv.clone) {
+    clone(argv.clone, function() {
+      console.log('[IFNO] Launched'.green)
+    });
+  } else {
+    console.log('[ERROR] Bad command, bye!'.red)
+    return;
+  }
+}
+exports.cli = cli;
+ 
+var clone = function(project, done) {
+  var Gitorious = require('gitoriou.js').Gitorious;
+  var config = {
+    url : 'http://gitorious.ow2.org',
+  }
+
+  var client = new Gitorious(config);
+  client.getProject(project, function(err, result) {
+    if (err) {
+      console.log('[ERROR] ' + err + ''.red)
+      return;
+    } else {
+      _.each(result.project.repositories.mainlines.repository, function(item) {
+        var child = exec('git clone ' + item.clone_url, {silent : false, async : true}, function(code, output) {
+          if (code != 0) {
+            console.log('[ERROR] ' + item.clone_url + ' : ' + output + ''.red)
+          } else {
+            console.log('[OK] ' + item.clone_url + ''.green);            
+          }
+        });
+      });
+      if (done) done();
+    }
+  })
+}
+exports.clone = clone;
